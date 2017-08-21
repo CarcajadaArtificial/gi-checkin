e67f6989859e8060c7af84458f896a38b8b34615
Daniel Gonzalez <daniel.gv.1993@gmail.com>
2017-08-21 12:01:01 -0500

    Deleting more unnecesary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
+
 Rails.application.routes.draw do
-  resources :tickets, except: [:index, :destroy] do
+  resources :tickets, except: [:index, :destroy, :edit] do
     get 'createBatch', :on => :collection
     get 'preregister', :on => :collection
     get 'register', :on => :collection
     get 'confirmation', :on => :collection
   end
-  resources :conferences, except: [:index]
+  resources :conferences, except: [:index, :edit, :destroy]
   resources :ticket_types, except: [:index]
   resources :events do
     get 'mktfest', :on => :collection
