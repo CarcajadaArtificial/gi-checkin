422d5358daf8de3796c9c5199537918a8ccf1be7
Daniel Gonzalez <daniel.gv.1993@gmail.com>
2017-08-25 21:04:37 -0500

    Fixed typo

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,5 +1,5 @@
 class TicketMailer < ApplicationMailer
-  default_from: 'no-reply@checkn.mx'
+  default from: 'no-reply@checkn.mx'
   def preregister_email(ticket)
     @ticket = ticket
     mail(to: @user.email, subject: 'Tu Boleto para ')
