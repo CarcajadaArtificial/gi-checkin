1a7bc6cafee25cc5f5cd7149ae04068bedba31bd
Oscar Alfonso Guerrero Nuñez <oaguerrero.95@gmail.com>
2017-08-31 01:10:48 -0500

    Styling preregister_email

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -6,10 +6,14 @@ class TicketMailer < ApplicationMailer
   def preregister_email(ticket)
     @ticket = ticket
     @barcode = Barby::Code128B.new(@ticket.ticket_reference)
-    @png = Barby::PngOutputter.new(@barcode).to_png(margin:20, height: 200, xdim: 2)
+    @png = Barby::PngOutputter.new(@barcode).to_png(margin:0, height: 200, xdim: 3)
     attachments.inline["barcode.png"] = @png
-    attachments.inline["mkt.png"] = File.read("#{Rails.root}/app/assets/images/mkt.png")
-    attachments.inline["barcode.png"] = @png
+    if @ticket.event.id == 1
+      attachments.inline["logo_event.png"] = File.read("#{Rails.root}/app/assets/images/mkt.png")
+    elsif @ticket.event.id == 2
+      attachments.inline["logo_event.png"] = File.read("#{Rails.root}/app/assets/images/esm.png")
+    end
+    attachments.inline["logo2.png"] = File.read("#{Rails.root}/app/assets/images/logo2.png")
 
     asunto = "Tu boleto para #{@ticket.event.event_name}"
     mail(to: @ticket.ticket_email, subject: asunto)
