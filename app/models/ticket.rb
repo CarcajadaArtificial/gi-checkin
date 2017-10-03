cb988c4dc18083bacc719e473e8244e0a175e6fa
Oscar Alfonso Guerrero Nuñez <oaguerrero.95@gmail.com>
2017-10-02 23:33:31 -0500

    HEROKU PUSH Changes to createBatch

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,17 +3,14 @@ class Ticket < ApplicationRecord
 
   def self.createTickets(typeId, quantity)
     n = Integer(quantity, 10)
-      reff = 20000
     for i in 1..n
     ff = 0
       while ff < 3 do
-        randomRef= SecureRandom.hex(3)
-        reff += 1
+        randomRef= SecureRandom.hex(4)
         ff += 1
-        if !(Ticket.exists?(:ticket_reference => reff))
-          puts "no existe"
+        if !(Ticket.exists?(:ticket_reference => randomRef))
           ff = 3
-          Ticket.create(:ticket_reference => reff, :ticket_ticketTypeId => typeId, :event_id => 1)
+          Ticket.create(:ticket_reference => randomRef, :ticket_ticketTypeId => typeId, :event_id => 3)
         else
 
         end
